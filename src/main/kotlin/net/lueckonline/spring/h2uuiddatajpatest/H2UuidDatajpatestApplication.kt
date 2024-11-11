9fbaf891be91b73c6c9a898ed4491755cf75cdf8
Michael Lück <michael.lueck@rewe-group.com>
2024-11-11 08:27:14 +0100

    execute query method when starting the application
    this has the same issue as in test

--- src/main/kotlin/net/lueckonline/spring/h2uuiddatajpatest/H2UuidDatajpatestApplication.kt
+++ src/main/kotlin/net/lueckonline/spring/h2uuiddatajpatest/H2UuidDatajpatestApplication.kt
@@ -2,10 +2,16 @@ package net.lueckonline.spring.h2uuiddatajpatest
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import java.util.UUID
 
 @SpringBootApplication
 class H2UuidDatajpatestApplication
 
 fun main(args: Array<String>) {
-    runApplication<H2UuidDatajpatestApplication>(*args)
+
+    println(UUID.randomUUID())
+    val context = runApplication<H2UuidDatajpatestApplication>(*args)
+    context.getBean(CustomRepository::class.java).nativeProjectingQuery().forEach {
+        println(it.id)
+    }
 }
--- src/test/kotlin/net/lueckonline/spring/h2uuiddatajpatest/CustomRepositoryTest.kt
+++ src/test/kotlin/net/lueckonline/spring/h2uuiddatajpatest/CustomRepositoryTest.kt
@@ -2,10 +2,10 @@ package net.lueckonline.spring.h2uuiddatajpatest
 
 import org.assertj.core.api.Assertions.assertThat
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import kotlin.test.Test
 
-@SpringBootTest
+@DataJpaTest
 class CustomRepositoryTest {
 
     @Autowired private lateinit var customRepository: CustomRepository
